ebeab3f9fa692f877c7980a70437d49fac7a0646
wcjeong <dncjf116@gmail.com>
2020-07-15 14:41:07 +0900

    [Spring IoC] Converter/Formatter 테스트 추가  - conversionWebMvcConfigurerConfig 수정

--- src/main/java/com/spring/ioc/ConversionService/CoversionWebMvcConfigurerConfig.java
+++ src/main/java/com/spring/ioc/ConversionService/CoversionWebMvcConfigurerConfig.java
@@ -1,14 +1,19 @@
-package com.spring.ioc.ConversionService;
-
-/* 해당 방식을 사용하는 것은 Converter 와 Formatter 를 따로 Bean 으로 등록하지 않는 기존 SpringFrameWork 의 방식이다.
-* Spring boot 에서는 WebConversionService 를 통해서 @Bean 으로 등록된 converter, formatter 를 자동으로 추가하여준다.*/
+//package com.spring.ioc.ConversionService;
+//
+//import com.spring.ioc.ConversionService.Convert.ConverterBean;
+//import com.spring.ioc.ConversionService.Formatter.FormatterBean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.format.FormatterRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///* 해당 방식을 사용하는 것은 Converter 와 Formatter 를 따로 Bean 으로 등록하지 않는 기존 SpringFrameWork 의 방식이다.
+//* Spring boot 에서는 WebConversionService 를 통해서 @Bean 으로 등록된 converter, formatter 를 자동으로 추가하여준다.*/
 //@Configuration
-//public class CoversionConfig implements WebMvcConfigurer {
+//public class CoversionWebMvcConfigurerConfig implements WebMvcConfigurer {
 //    @Override
 //    public void addFormatters(FormatterRegistry registry) {
 //        registry.addFormatter(new FormatterBean());
 //        registry.addConverter(new ConverterBean.ConverterToString());
 //        registry.addConverter(new ConverterBean.StringToConverter());
 //    }
-//
 //}
